17b3a24fa786551e9596fdcf11354fbff6507105
Mmek Ekon <102446131+KokoScripts@users.noreply.github.com>
2025-07-14 10:47:19 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -73,7 +73,9 @@ class TestMemoize(unittest.TestCase):
                 """Returns result of a_method, memoized."""
                 return self.a_method()
 
-        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+        with patch.object(
+            TestClass, "a_method", return_value=42
+        ) as mock_method:
             obj = TestClass()
             self.assertEqual(obj.a_property, 42)
             self.assertEqual(obj.a_property, 42)
